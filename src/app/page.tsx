37e4dd66a8645db3a1a44d9483a2a250ab291d05
sana arshad <160604463+Aaira123@users.noreply.github.com>
2024-11-10 04:01:04 -0800

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,96 @@
+
 import Image from "next/image";
+import { FaFacebookF } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <main>
+      <div className="container pt-8">
+        <div className="flex justify-between items-baseline">
+          <div text-xl font-medium text-black> Logo</div>
+          <ul className="gap-10 lg:gap-16 hidden md:flex text-black">
+            <li className="menuLink"><a href="#hero">Home</a></li>
+            <li className="menuLink"><a href="#about">About</a></li>
+            <li className="menuLink"><a href="#contact">Contact</a></li>
+          </ul>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+      </div>
+      <div id="hero" className="min-h-screen bg-[url(/pic1.jpeg)] bg-cover" 
+      style={{backgroundSize: "80%", backgroundPosition: "left 100px"}}
+      >
+        <div className="container grid lg:grid-cols-2 h-[calc(100vh-60)]">
+          <div className="hidden lg:block"></div>
+          <div className="text-[80px] sm:text-[100px] font-bold leading-tight flex justify-end items-baseline"></div>
+          <div>
+            <h1 className="text-8xl mg:3xl text-end font-serif text-yellow-600"> New World</h1>
+            <p className="text-end text-7xl font-semibold text-yellow-700"> Enjoy the Music with friend</p>
+          </div>
+          
+
+        </div>
+      </div>
+      
+        <div id='contact' className='pt-32 container'>
+        <div className='grid md:grid-cols-2 gap-10'>
+            <div className='space-y-8'>
+                <h2 className='text-5xl'>Get in Touch</h2>
+                <p className='text-gray-900 text-[18px] pt-2'>
+                    Drop me a line, give me a call, or send me a message by submitting the form.
+                </p>
+                <div className='flex gap-3 items-center'>
+                 abc@gmail.com
+                </div>
+                <div className='flex gap-3 items-center'>
+              (021)000-0000
+                </div>
+            </div>
+            <div className='space-y-8'>
+                <div className='flex flex-col gap-1'>
+                    <label htmlFor='name'>Name</label>
+                    <input type='text'
+                    className='h-[40px] bg-transparent border border-black'
+                    id='name'/>
+                </div>
+                <div className='flex flex-col gap-1'>
+                    <label htmlFor='email'>Email</label>
+                    <input type='text'
+                    className='h-[40px] bg-transparent border border-black'
+                    id='email'/>
+                    </div>
+                    <div className='flex flex-col gap-1'>
+                    <label htmlFor='msg'>Message</label>
+                    <textarea
+                    className='bg-transparent border border-black'
+                    id='msg' rows={8}>
+                    </textarea>
+                </div>
+                <button className='bg-white p-2 px-6'>Send</button>
+            </div>
+        </div>
+      
     </div>
-  );
+          
+        
+      
+      <div id="about" className="container pt-32">
+        <h2 className="text-4xl mg:5xl"> About Me</h2>
+        <p className="text-gray-1000 pt-4"> I am student at GIAIC ,pursing a curse in artifical Intelligence,WEB 3.0, & Metaverse passionate about technology and constantly learning new skills to stay up to-date with the latest innovation</p>
+      </div>
+
+      <div className="text-center">
+            <button className="bg-slate-200  mt-10 p-2 px-4 text-justify"> Join Now</button>
+          </div>
+
+      <div className="mt-16 py-6 text-center bg-accent">
+          &copy; Sana| All right reserved
+          <FaFacebookF />
+          
+          <FaFacebook />
+      </div>
+    </main>
+  )
+    
 }
